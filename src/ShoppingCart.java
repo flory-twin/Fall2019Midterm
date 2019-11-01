a94eb58468a93912b67789fa3515522f35359952
graffadamr <graff.adam.r@gmail.com>
2019-11-01 11:45:45 -0400

    added some maths ag

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -23,110 +23,135 @@ import java.util.HashMap;
       -ArrayList of -potential- products (the inventory)
       -HashMap<CatsProduct, int> (the actual cart) 
  */
-public class ShoppingCart
-{
+public class ShoppingCart {
 
-   private ArrayList<CatsProduct> inventory;
-   private HashMap<CatsProduct, Integer> cart;
-   
-   public ShoppingCart() {
-      super();
-      // TODO Auto-generated constructor stub
-      inventory = new ArrayList<>();
-      cart = new HashMap<>();
-      
-      
-   }
+	private ArrayList<CatsProduct> inventory;
+	private HashMap<CatsProduct, Integer> cart;
 
-   //TODO Change back from public to private once testing is done
-   public void readCatsFromCSV(String fileName) {
+	public ShoppingCart() {
+		super();
+		// TODO Auto-generated constructor stub
+		inventory = new ArrayList<>();
+		cart = new HashMap<>();
 
-      // Open the file for reading
-      Path path = Paths.get("resources", fileName);
+	}
 
-      File file = path.toFile();
+	// TODO Change back from public to private once testing is done
+	public void readCatsFromCSV(String fileName) {
 
-      try {
-         BufferedReader br = new BufferedReader(new FileReader(file));
-         String line = br.readLine();
-         // One-at-a-time, read each line in the file.
-         while (line != null) {
-            // For each line,
-            // Create a CatsProduct from the line and
-            CatsProduct theCatOnThisLine = CatsProduct.fromString(line);
-            // Add it to the list.
-            inventory.add(theCatOnThisLine);
-            // Now read a new line.
-            line = br.readLine();
-         }
+		// Open the file for reading
+		Path path = Paths.get("resources", fileName);
 
-         br.close();
-      } catch (FileNotFoundException e) {
-         System.out.println("Something went wrong with the file.");
-      } catch (IOException e) {
-         System.out.println("Something went wrong when we tried to read the file.");
+		File file = path.toFile();
 
-      }
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line = br.readLine();
+			// One-at-a-time, read each line in the file.
+			while (line != null) {
+				// For each line,
+				// Create a CatsProduct from the line and
+				CatsProduct theCatOnThisLine = CatsProduct.fromString(line);
+				// Add it to the list.
+				inventory.add(theCatOnThisLine);
+				// Now read a new line.
+				line = br.readLine();
+			}
 
-   }
+			br.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Something went wrong with the file.");
+		} catch (IOException e) {
+			System.out.println("Something went wrong when we tried to read the file.");
+
+		}
+
+	}
 //   calculateSalesTax()
 //   calculateSubTotal()
 //   calculateGrandTotal()
 //   addItemToCart()
 //   removeItemFromCart()
 //   displayCart()
-   
-   public static void main(String[] args) {
-      ShoppingCart thisCartInstance = new ShoppingCart();
-      // This -uses- a prison cell. 
-      // Other people call it a "method call" or "calling the method." 
-      // There might be an equals in front (like int a = scan.nextInt();)
-      //     That's a dead giveaway if it's there.
-      // Even if there isn't, (1) it will NEVER have moustaches and (2) it will ALWAYS be inside another method.
-      thisCartInstance.readCatsFromCSV("Cats.csv");
-      System.out.println(thisCartInstance.inventory);
-      
-   }
-   
-   // This defines a prison cell; other people call it a "method definition."
-   //   It's the only one which can have public, static, or BigDecimal directly in front of the name.
-   //   It's also the only one with moustaches.
-   //   It can NEVER occur inside another method.
-  public BigDecimal calcTotalBeforeTax() { 
-	  
-	  // All the loops are jumprope/loop-de-loop. 
-	  // To extend the torture example...
-	  //  * The people holding the rope set the rules! They have two main types:
-	  //  * The -normal- for loop is a simple counting game;
-	  //      the people holding the rope make everyone who is tortured jump 250 or 500 or 20,000 before 
-	  //      you can stop. I guess they do it with whips...it's like a rack in the prison cell.
-	  //  * The -normal- while loop is even ruder;
-	  //      you have to go until they're satisfied.
-	  //      They'll tell you what will satisfy them...but since 
-	  //      we're putting different prisoners through the torture, different
-	  //      prisoners take different counts to do it. 
-	  //      The torturers might play favorites and let some prisoners off light...
-	  //  * The ENHANCED for loop is still a simple counting game.
-	  //      However, the torturers are not yelling the number at you.
-	  //      Instead, they've got an array or a list or something, and they're 
-	  //      going through it one by one, and yelling the array content at you.
-	  //        You'll still get out in a set number of jumps.
-	  //      This looks like:
-	  //                  for (Thing nameForTheThing : aBunchOfThings) {
-	  //                       yell (nameForTheThing);
-	  //                  }
-	  //  
-	  
-	  BigDecimal subTotal = new BigDecimal(0.0);
-	  
-	  for (CatsProduct entry : cart.keySet()) {
-		    System.out.println(entry + " = " + cart.get(entry));
-		    subTotal = subTotal.add(
-		    		(entry.getPrice().multiply(
-		    				new BigDecimal(
-		    						cart.get(entry)))));
+
+	public static void main(String[] args) {
+		ShoppingCart thisCartInstance = new ShoppingCart();
+		// This -uses- a prison cell.
+		// Other people call it a "method call" or "calling the method."
+		// There might be an equals in front (like int a = scan.nextInt();)
+		// That's a dead giveaway if it's there.
+		// Even if there isn't, (1) it will NEVER have moustaches and (2) it will ALWAYS
+		// be inside another method.
+		thisCartInstance.readCatsFromCSV("Cats.csv");
+		System.out.println(thisCartInstance.inventory);
+		CatsProduct catTest = new CatsProduct("Russian Blue", "Voika", "Silvery-blue cat.", new BigDecimal(600));
+		thisCartInstance.addItem(catTest);
+		System.out.println(thisCartInstance.cart);
+		thisCartInstance.addItem(catTest);
+		System.out.println(thisCartInstance.cart);
+
+	}
+
+	// This defines a prison cell; other people call it a "method definition."
+	// It's the only one which can have public, static, or BigDecimal directly in
+	// front of the name.
+	// It's also the only one with moustaches.
+	// It can NEVER occur inside another method.
+	public BigDecimal calcTotalBeforeTax() {
+
+		// All the loops are jumprope/loop-de-loop.
+		// To extend the torture example...
+		// * The people holding the rope set the rules! They have two main types:
+		// * The -normal- for loop is a simple counting game;
+		// the people holding the rope make everyone who is tortured jump 250 or 500 or
+		// 20,000 before
+		// you can stop. I guess they do it with whips...it's like a rack in the prison
+		// cell.
+		// * The -normal- while loop is even ruder;
+		// you have to go until they're satisfied.
+		// They'll tell you what will satisfy them...but since
+		// we're putting different prisoners through the torture, different
+		// prisoners take different counts to do it.
+		// The torturers might play favorites and let some prisoners off light...
+		// * The ENHANCED for loop is still a simple counting game.
+		// However, the torturers are not yelling the number at you.
+		// Instead, they've got an array or a list or something, and they're
+		// going through it one by one, and yelling the array content at you.
+		// You'll still get out in a set number of jumps.
+		// This looks like:
+		// for (Thing nameForTheThing : aBunchOfThings) {
+		// yell (nameForTheThing);
+		// }
+		//
+
+		BigDecimal subTotal = new BigDecimal(0.0);
+
+		for (CatsProduct entry : cart.keySet()) {
+			System.out.println(entry + " = " + cart.get(entry));
+			subTotal = subTotal.add((entry.getPrice().multiply(new BigDecimal(cart.get(entry)))));
 		}
-	return subTotal;
-	  
-   }
+		return subTotal;
+	}
+
+	public void removeItem (CatsProduct thingToDelete) {
+  		
+		cart.remove(thingToDelete);
+		
+	}
+	
+	public void addItem (CatsProduct thingToAdd) {
+		
+			if (cart.containsKey(thingToAdd)) {
+				int currentCount = cart.get(thingToAdd);
+				int newCount = currentCount + 1;
+				cart.put(thingToAdd, newCount);		
+			}
+			
+			else  { 
+				cart.put(thingToAdd, 1);
+			}
+  		
+
+  	}
+
 }
